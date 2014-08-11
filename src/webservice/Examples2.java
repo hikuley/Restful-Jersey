80ded898b38e914e81097b1da15a1270d0198b5f
Halil İbrahim Küley <ssoulless25@gmail.com>
2014-08-11 14:47:17 +0300

    example2 post methods

--- src/webservice/Examples2.java
+++ src/webservice/Examples2.java
@@ -1,9 +1,10 @@
 package webservice;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import models.People;
+
+import javax.ws.rs.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by halil on 11.08.2014.
@@ -13,17 +14,45 @@ public class Examples2 {
 
 
     @GET
-    @Produces("text/html")
+    @Produces("text/plain")
     public String parthRootService() {
         return "Root Path";
     }
 
 
     @GET
-    @Produces
+    @Produces("text/plain")
     @Path("/getUser/{userName}")
     public String getExampleMetot(@PathParam("userName") String userName) {
         return userName;
+    }
+
+    @POST
+    @Consumes("application/x-www-form-urlencoded")
+    public String postParamerter(@FormParam("name") String name, @FormParam("surname") String surname) {
+        return " Hoşgeldin " + name + " " + surname;
+    }
+
+    @POST
+    @Path("/userJson")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces("application/json")
+    public People postParamerter2(
+            @FormParam("id") int id,
+            @FormParam("name") String name,
+            @FormParam("surname") String surname,
+            @FormParam("gender") String gender
+    ) {
+        People people = new People();
+        List<String> hobies = new ArrayList<String>();
+        people.setId(id);
+        people.setName(name);
+        people.setSurname(surname);
+        people.setGender(gender);
+        hobies.add("Fenerbahçe");
+        hobies.add("Playstation");
+        people.setHobies(hobies);
+        return people;
     }
 
 
